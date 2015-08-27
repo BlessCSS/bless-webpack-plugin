9a11f1544a2f1dce5155719221ec57a98a604405
Tiddo Langerak <tiddolangerak@gmail.com>
2015-08-27 15:59:08 +0200

    Added sourcemap support
    Fly-by:
    - Fixed readme
    - Moved test folder to demo folder (it doesn't contain any actual tests)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { chunk } from 'bless';
-import { basename, extname } from 'path';
+import { basename, extname, resolve } from 'path';
 import RawSource from 'webpack/lib/RawSource';
 
 /**
@@ -24,23 +24,40 @@ function generateImports(sourceFileName, numberOfImports) {
 /**
  * Creates an array of chunk objects from an object returned by bless.chunk
  */
-function createFileChunks(sourceFileName, blessed) {
-	return blessed.data.map((chunk, index) => {
+function createChunks(fileName, source, sourcemaps, context) {
+	const sourceFileName = fileName.replace(/\.css$/, '.source.css');
+	const options = {
+		sourcemaps,
+		source : sourceFileName
+	};
+	const blessed = chunk(source, options);
+
+	const chunks = blessed.data.map((chunk, index) => {
 		let name;
 		if (index === blessed.data.length - 1) {
-			name = sourceFileName;
-			const imports = generateImports(sourceFileName, blessed.data.length - 1) ;
+			name = fileName;
+			const imports = generateImports(fileName, blessed.data.length - 1) ;
 			chunk = `${imports}\n${chunk}`;
 		} else {
-			name = generateFileName(sourceFileName, index + 1);
+			name = generateFileName(fileName, index + 1);
 		}
-		return { name, chunk, map : blessed.maps[index] };
+		if (sourcemaps) {
+			chunk += `\n/*# sourceMappingURL=${name}.map */`;
+		}
+		return { name, content: chunk, map : blessed.maps[index] };
 	});
+
+	if (sourcemaps) {
+		chunks.push({
+			name : sourceFileName,
+			content : source
+		});
+	}
+
+	return chunks;
 }
 
-export default function(options, pattern) {
-	pattern = pattern || /\.css$/;
-	options = options || {};
+export default function(sourceMaps = false) {
 
 	return {
 		apply(compiler) {
@@ -48,12 +65,16 @@ export default function(options, pattern) {
 				compilation.plugin('optimize-assets', (assets, callback) => {
 
 					Object.keys(assets)
-						.filter(assetName => pattern.test(assetName))
+						.filter(assetName => /\.css$/.test(assetName))
 						.map(fileName => [fileName, assets[fileName].source()])
-						.map(([fileName, source]) => [fileName, chunk(source, options)])
-						.map(([fileName, blessed]) => createFileChunks(fileName, blessed))
+						.map(([fileName, source]) => createChunks(fileName, source, sourceMaps))
 						.reduce((a, b) => [...a, ...b]) //flatten
-						.forEach(({ name, chunk }) => assets[name] = new RawSource(chunk));
+						.forEach(({ name, content, map }) => {
+							assets[name] = new RawSource(content);
+							if (map) {
+								assets[`${name}.map`] = new RawSource(JSON.stringify(map));
+							}
+						});
 
 					callback();
 				});
